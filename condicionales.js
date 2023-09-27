40d933e5144574ac6048ae6f10a01e3d90489e1e
Diego Rojas <diego.rojas@symplifica.com>
2023-09-27 10:07:13 -0500

    Game to the rock paper scissors

--- condicionales.js
+++ condicionales.js
@@ -29,39 +29,33 @@ console.log(resultado);
 
 
 // ######################################################### Rock, Paper, Scissors Game ##########################
-function jugar() {
-    // Prompt the user to choose an option: rock, paper, or scissors, and convert their input to lowercase.
-    const opcionUsuario = prompt("Choose an option: rock, paper, or scissors").toLowerCase();
 
-    // Check if the user's choice is valid (rock, paper, or scissors).
-    if (opcionUsuario === "rock" || opcionUsuario === "paper" || opcionUsuario === "scissors") {
-        // Create an array of options for the computer to choose from.
-        const opciones = ["rock", "paper", "scissors"];
-        // Generate a random index to select an option for the computer.
-        const opcionMaquina = opciones[Math.floor(Math.random() * 3)];
+const optionUser = prompt("Choose an option: rock, paper, or scissors").toLowerCase();
+function game(){
 
-        // Display the user's choice and the computer's choice.
-        console.log(`Your choice: ${opcionUsuario}`);
-        console.log(`Computer's choice: ${opcionMaquina}`);
-
-        // Determine the game result based on the choices.
-        if (opcionUsuario === opcionMaquina) {
-            console.log("It's a tie");
-        } else if (
-            (opcionUsuario === "rock" && opcionMaquina === "scissors") ||
-            (opcionUsuario === "paper" && opcionMaquina === "rock") ||
-            (opcionUsuario === "scissors" && opcionMaquina === "paper")
-        ) {
-            console.log("You win!");
-        } else {
-            console.log("You lose");
-        }
+    if (optionUser === "rock" || optionUser === "paper" || optionUser === "scissors"){
+        const option = ["rock", "paper", "scissors"]
+        const machineOption = option[Math.floor(Math.random() * 3)];
+        console.log(`Your choice: ${optionUser}`);
+        console.log(`Computer's choice: ${machineOption}`);
+    if (optionUser === machineOption) {
+        console.log("It's a tie");
+    } else if (
+        (optionUser === "rock" && machineOption === "scissors") ||
+        (optionUser === "paper" && machineOption === "rock") ||
+        (optionUser === "scissors" && machineOption === "paper")
+    ) {
+        console.log("You win!");
     } else {
-        // If the user's choice is not valid, display an error message.
-        console.log("Invalid option. Please choose rock, paper, or scissors.");
+        console.log("You lose");
+    }
+    }else {
+        console.log("Invalid option. Please choose rock, paper, or scissors.")
     }
 }
+game("rock");
+game("paper");
+game("scissors");
 
-// Call the function to start the game.
-jugar();
+
 
