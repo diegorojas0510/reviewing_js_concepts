e02166adcee7eefff228c306490dcd23b99d36fb
Diego Rojas <diego.rojas@symplifica.com>
2024-03-05 10:15:42 -0500

    Change key a cat and change text tittle

--- api_rest/main.js
+++ api_rest/main.js
@@ -1,9 +1,8 @@
 // pruebas de funcionamiento
 console.log('Hello, word');
 
-const API_URL_RAMDOM = "https://api.thedogapi.com/v1/images/search?limit=2&";
-const API_URL_FAVOTITES = "https://api.thedogapi.com/v1/images/search?favourites?limit=2&"
-const API_KEY = "api_key=live_TQbkEDPisyPb0Kz6NGkKcKJApq4ScCC7Q5E9mA6jQII4C6hCaQnzhS3wNXhKctfL"
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 // reconociendo mi url
 /*
 HTTPS = Protocolo de transferencia de datos de manera segura (Protocolo de Transferencia de Hipertexto Seguro):
@@ -27,12 +26,12 @@ const statusCodes = {
     404: "Not Found",
     500: "Internal Server Error"
 };
-async function loadRandomDogs() {
+async function loadRandomCats() {
     try {
-        const response = await fetch(`${API_URL_RAMDOM}${API_KEY}`);
+        const response = await fetch(API_URL_RANDOM);
         // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
         const data = await response.json();
-        console.log('Cargar perros');
+        console.log('Cargar Michis');
         console.log(data);
         //const img = document.querySelector('img');
 
@@ -41,26 +40,22 @@ async function loadRandomDogs() {
         }else {
             const img1 = document.getElementById('img1');
             const img2 = document.getElementById('img2');
-            const img3 = document.getElementById('img3');
             img1.src = data[0].url;
             img2.src = data[1].url;
-            img3.src = data[2].url;
         }
 
     } catch (error) {
-        console.error('Error fetching Dog image:', error);
-        spanError.innerHTML = "Error los perros no se pueden cargar aleatoreamente." +error.message;
+        console.error('Error fetching Cat image:', error);
+        spanError.innerHTML = "Error los gatos no se pueden cargar aleatoreamente." +error.message;
     }
 }
-loadRandomDogs();
 
-
-async function loadFavouritesDogs() {
+async function loadFavouritesCats() {
     try {
-        const response = await fetch(`${API_URL_FAVOTITES}${API_KEY}`);
+        const response = await fetch(API_URL_FAVOTITES);
         // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
         const data = await response.json();
-        console.log('Perros Favoritos');
+        console.log('Gatos Favoritos');
         console.log(data);
 
         if (response.status !== 200){
@@ -72,9 +67,8 @@ async function loadFavouritesDogs() {
         spanError.innerHTML = "Hubo un error al cargar los perros." + error.message;
     }
 }
-loadFavouritesDogs();
 
-async function saveFavouriteDogs(imageId) {
+async function saveFavouriteCats() {
     try {
         const response = await fetch(API_URL_FAVOTITES, {
             method: 'POST',
@@ -82,23 +76,20 @@ async function saveFavouriteDogs(imageId) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                image_id: imageId,
+                image_id: 'cbt',
             }),
         });
 
         const data = await response.json();
-        console.log('Guardar Perros Favoritos');
-        console.log(data);
+        console.log('Guardar Michis Favoritos');
         console.log(response);
 
         if (response.status !== 200) {
-            spanError.innerHTML = `Hubo un error al guardar en favoritos: ${response.status} ${data.message}`;
-        } else {
-            // Mostrar mensaje de éxito
+            spanError.innerHTML = `Hubo un error al guardar los gatos en favoritos: ${response.status} ${data.message}`;
         }
     } catch (error) {
         console.error('Error:', error);
-        spanError.innerHTML = `Hubo un error al guardar los perros en favoritos. ${error.message}`;
+        spanError.innerHTML = `Hubo un error al guardar los gatos en favoritos. ${error.message}`;
     }
 }
 
@@ -106,11 +97,11 @@ async function saveFavouriteDogs(imageId) {
 
 // Asignar el evento click al botón para obtener una nueva imagen
 const button = document.getElementById('reload');
-button.addEventListener('click', loadRandomDogs);
+button.addEventListener('click', loadRandomCats);
 
 // Ejecutar la función al cargar la página
-//document.addEventListener('DOMContentLoaded', loadRandomDogs);
-//document.addEventListener('DOMContentLoaded', loadFavouritesDogs);
+document.addEventListener('DOMContentLoaded', loadRandomCats);
+document.addEventListener('DOMContentLoaded', loadFavouritesCats);
 
 
 
