9d9069e81f8ad8050cab07a32e7a280ca1af494c
Diego Rojas <diego.rojas@symplifica.com>
2024-03-04 19:12:42 -0500

    Add funtion favorites dog

--- api_rest/main.js
+++ api_rest/main.js
@@ -1,7 +1,8 @@
 // pruebas de funcionamiento
 console.log('Hello, word');
 
-const URL = "https://api.thedogapi.com/v1/images/search?limit=10";
+const API_URL_RAMDOM = "https://api.thedogapi.com/v1/images/search?limit=2&api_key=";
+const API_URL_FAVOTITES = "https://api.thedogapi.com/v1/images/search?favourites?limit=2&api_key="
 // reconociendo mi url
 /*
 HTTPS = Protocolo de transferencia de datos de manera segura (Protocolo de Transferencia de Hipertexto Seguro):
@@ -9,49 +10,69 @@ Host y Endpoint api.thedogapi.com este es el servidor al que se le hace la solic
 Ruta v1/images/search: este enpoint es la ruta que maneja la solicitud especifica
 ?: indica el inicio de de lo párametros de consulta en este caso limit=10
 Limit=10: Este párametro limita el número de resultados que se devuelven en la respuesta de la api cantidad de perros 10
+&: Sirve parta concatenar
+api_key: le pasamos la key para autenticarnos en nuestra aplicación con esto la api sabra quienes somos y si tenemos limitantes nos empesara a contar las peticiones
+en este caso son 10.mil
 * */
-
 // Función asíncrona que realiza la petición a la API eliminando los multiples usos de .then y siendo más legible la lectura de la función
 // la función asincrona en js permite realizar tareas que pueden llevar tiempo sin bloquar la ejecución del resto del código
-async function fetchDogImage() {
+
+const spanError = document.getElementById('error');
+
+async function loadRandomDogs() {
     try {
-        const response = await fetch(URL);
+        const response = await fetch(API_URL_RAMDOM);
         // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
         const data = await response.json();
+        console.log('Cargar perros');
         console.log(data);
         //const img = document.querySelector('img');
-        const img1 = document.getElementById('img1');
-        const img2 = document.getElementById('img2');
-        const img3 = document.getElementById('img3');
-        const img4 = document.getElementById('img4');
-        const img5 = document.getElementById('img5');
-        const img6 = document.getElementById('img6');
-        const img7 = document.getElementById('img7');
-        const img8 = document.getElementById('img8');
-        const img9 = document.getElementById('img9');
-        const img10 = document.getElementById('img10');
 
-        img1.src = data[0].url;
-        img2.src = data[1].url;
-        img3.src = data[2].url;
-        img4.src = data[3].url;
-        img5.src = data[4].url;
-        img6.src = data[5].url;
-        img7.src = data[6].url;
-        img8.src = data[7].url;
-        img9.src = data[8].url;
-        img10.src = data[9].url;
+        if (response.status !== 200){
+            spanError.innerHTML = "Hubo un error: " + response.status + error.message;
+        }else {
+            const img1 = document.getElementById('img1');
+            const img2 = document.getElementById('img2');
+            const img3 = document.getElementById('img3');
+            const img4 = document.getElementById('img4');
+            const img5 = document.getElementById('img5');
 
+            img1.src = data[0].url;
+            img2.src = data[1].url;
+            img3.src = data[2].url;
+            img4.src = data[3].url;
+            img5.src = data[5].url;
+        }
 
     } catch (error) {
-        console.error('Error fetching cat image:', error);
+        console.error('Error fetching Dog image:', error);
     }
 }
 
-// Ejecutar la función al cargar la página
-document.addEventListener('DOMContentLoaded', fetchDogImage);
+async function favoritesRandomDogs() {
+    try {
+        const response = await fetch(API_URL_FAVOTITES);
+        // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
+        const data = await response.json();
+        console.log('Perros Favoritos');
+        console.log(data);
+
+        if (response.status !== 200){
+            spanError.innerHTML = "Hubo un error: " + response.status + error.message;
+        }
+
+    } catch (error) {
+        console.error('Error :', error);
+    }
+}
 
 // Asignar el evento click al botón para obtener una nueva imagen
-const button = document.querySelector('button');
-button.addEventListener('click', fetchDogImage);
+const button = document.getElementById('reload');
+button.addEventListener('click', loadRandomDogs);
+
+// Ejecutar la función al cargar la página
+document.addEventListener('DOMContentLoaded', loadRandomDogs);
+document.addEventListener('DOMContentLoaded', favoritesRandomDogs);
+
+
 
