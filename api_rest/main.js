bcbb3775db359878d4bf296d5691855f886a41f7
Diego Rojas <diego.rojas@symplifica.com>
2024-03-05 00:26:01 -0500

    Add funtions save dogs

--- api_rest/main.js
+++ api_rest/main.js
@@ -1,8 +1,9 @@
 // pruebas de funcionamiento
 console.log('Hello, word');
 
-const API_URL_RAMDOM = "https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_TQbkEDPisyPb0Kz6NGkKcKJApq4ScCC7Q5E9mA6jQII4C6hCaQnzhS3wNXhKctfL";
-const API_URL_FAVOTITES = "https://api.thedogapi.com/v1/images/search?/favourites?limit=1&api_key=live_TQbkEDPisyPb0Kz6NGkKcKJApq4ScCC7Q5E9mA6jQII4C6hCaQnzhS3wNXhKctfL"
+const API_URL_RAMDOM = "https://api.thedogapi.com/v1/images/search?limit=2&";
+const API_URL_FAVOTITES = "https://api.thedogapi.com/v1/images/search?favourites?limit=2&"
+const API_KEY = "api_key=live_TQbkEDPisyPb0Kz6NGkKcKJApq4ScCC7Q5E9mA6jQII4C6hCaQnzhS3wNXhKctfL"
 // reconociendo mi url
 /*
 HTTPS = Protocolo de transferencia de datos de manera segura (Protocolo de Transferencia de Hipertexto Seguro):
@@ -28,7 +29,7 @@ const statusCodes = {
 };
 async function loadRandomDogs() {
     try {
-        const response = await fetch(API_URL_RAMDOM);
+        const response = await fetch(`${API_URL_RAMDOM}${API_KEY}`);
         // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
         const data = await response.json();
         console.log('Cargar perros');
@@ -41,14 +42,9 @@ async function loadRandomDogs() {
             const img1 = document.getElementById('img1');
             const img2 = document.getElementById('img2');
             const img3 = document.getElementById('img3');
-            const img4 = document.getElementById('img4');
-            const img5 = document.getElementById('img5');
-
             img1.src = data[0].url;
             img2.src = data[1].url;
             img3.src = data[2].url;
-            img4.src = data[3].url;
-            img5.src = data[4].url;
         }
 
     } catch (error) {
@@ -59,9 +55,9 @@ async function loadRandomDogs() {
 loadRandomDogs();
 
 
-async function favoritesDogs() {
+async function loadFavouritesDogs() {
     try {
-        const response = await fetch(API_URL_FAVOTITES);
+        const response = await fetch(`${API_URL_FAVOTITES}${API_KEY}`);
         // await es para esperar que una operación asíncrona, como una solicitud de red o una lectura de archivo, se complete antes de continuar ejecutando el resto del código en la función.
         const data = await response.json();
         console.log('Perros Favoritos');
@@ -73,10 +69,40 @@ async function favoritesDogs() {
 
     } catch (error) {
         console.error('Error :', error);
-        spanError.innerHTML = "Hubo un error al cargar los perros." +error.message;
+        spanError.innerHTML = "Hubo un error al cargar los perros." + error.message;
     }
 }
-favoritesDogs();
+loadFavouritesDogs();
+
+async function saveFavouriteDogs(imageId) {
+    try {
+        const response = await fetch(API_URL_FAVOTITES, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                image_id: imageId,
+            }),
+        });
+
+        const data = await response.json();
+        console.log('Guardar Perros Favoritos');
+        console.log(data);
+        console.log(response);
+
+        if (response.status !== 200) {
+            spanError.innerHTML = `Hubo un error al guardar en favoritos: ${response.status} ${data.message}`;
+        } else {
+            // Mostrar mensaje de éxito
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        spanError.innerHTML = `Hubo un error al guardar los perros en favoritos. ${error.message}`;
+    }
+}
+
+
 
 // Asignar el evento click al botón para obtener una nueva imagen
 const button = document.getElementById('reload');
@@ -84,7 +110,7 @@ button.addEventListener('click', loadRandomDogs);
 
 // Ejecutar la función al cargar la página
 //document.addEventListener('DOMContentLoaded', loadRandomDogs);
-//document.addEventListener('DOMContentLoaded', favoritesDogs);
+//document.addEventListener('DOMContentLoaded', loadFavouritesDogs);
 
 
 
